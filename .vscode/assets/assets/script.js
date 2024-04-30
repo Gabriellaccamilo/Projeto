4fae959c7dc0dd7b2cabead7269a16c26cd2c5b4
Gabriella <gcamilodev@gmail.com>
2024-04-30 17:33:06 -0300

    ultimas alterações necessárias. Agora com as minhas imgs e nome.

--- .vscode/assets/assets/script.js
+++ .vscode/assets/assets/script.js
@@ -5,10 +5,10 @@ function toggleMode() {
     const img = document.querySelector("#profile img")
 
     if (html.classList.contains("light")){
-      img.setAttribute("src", "./assets/assets/avatar-light.png")
+      img.setAttribute("src", "./assets/assets/TESTE2.ORIG.png")
       } else {
-        img.setAttribute("src", "./assets/assets/avatar.png")
+        img.setAttribute("src", "./assets/assets/TESTE.png")
       }
 
   
-}
+} 
